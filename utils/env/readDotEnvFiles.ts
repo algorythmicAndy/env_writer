471cd3f24bc90fd62fce621931b4d4b20cc893d2
algorythmicAndy <andrew.m.donovan@ntlworld.com>
2026-09-21 01:04:52 +0100

    fix(read stream): abstracted path resolve, DRY violation

--- utils/env/readDotEnvFiles.ts
+++ utils/env/readDotEnvFiles.ts
@@ -2,17 +2,18 @@ import { createReadStream, existsSync, writeFileSync } from 'node:fs';
 import streamToBuffer from './streamToBuffer';
 import path from 'node:path';
 import readPreserveLocalSecrets from './readPreserveLocalSecrets';
-
+const envResolve = path.resolve('.env');
+const sampleResolve = path.resolve('.env.sample');
 const StableLineBreak = Buffer.from('/n');
 export async function readDotEnvFiles(): Promise<null | Buffer> {
-  if (!existsSync('/.env')) {
+  if (!existsSync(envResolve)) {
     writeFileSync('/.env', '');
   }
 
   return new Promise(async (resolve, reject) => {
-    const localSecrets = await readPreserveLocalSecrets(path.resolve('.env'));
+    const localSecrets = await readPreserveLocalSecrets(envResolve);
     const secretsBuffer = Buffer.from(localSecrets.join());
-    const sampleRead = createReadStream(path.resolve('.env.sample'));
+    const sampleRead = createReadStream(sampleResolve);
     const sampleBuffer = await streamToBuffer(sampleRead);
     if (!sampleBuffer) return reject;
     resolve(
